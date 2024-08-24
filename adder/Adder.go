abac6c70ab82b959ca731247ad6ebad42f6feb68
tsegaywelu <tsegaygebrezgi56@gmail.com>
2024-08-24 22:13:37 +0300

    this is what i am sending

--- adder/Adder.go
+++ adder/Adder.go
@@ -1,6 +1,11 @@
 package adder
 
+import "fmt"
+
 func Adder(x, y int) int {
 	reult := x + y
+	fmt.Println("**************************Tsegay Welu Add function*******************************")
+
+	fmt.Println("*****************************we are sending the sum of the cumbers ************************************")
 	return reult
 }
